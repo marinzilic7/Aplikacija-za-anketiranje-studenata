0832ed3f956342872d5c71fc318a36e623fa9a6e
Contineo <marinzilic507@gmail.com>
2023-07-20 00:29:34 +0200

    registerUser complete

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use function Symfony\Component\String\b;
+
+class UserController extends Controller
+{
+    public function regUser(Request $request)
+    {
+
+        $data = $request->validate(
+            [
+                'name' => 'required',
+                'lastName' => 'required',
+                'email' => 'required|email',
+                'password' => 'required|min:5|regex:/^[A-Z]/',
+                'passwordConf' => 'required|same:password'
+            ],
+            [
+                'name.required' => "Ime je obavezno",
+                'lastName.required' => "Prezime je obavezno ",
+                'email.required' => 'Email je obavezan',
+                'email.email' => 'Unesite ispravan format email adrese.',
+                'password.required' => 'Lozinka je obavezna',
+                'password.min' => 'Lozinka mora imati minimalno 5 znakova ',
+                'passwordConf.required' => 'Potvrda lozinke je obavezna',
+                'passwordConf.same' => 'Vasa lozinka i potvrda vase lozinke nisu iste!',
+                'password.regex' => 'Vasa lozinka mora zapoceti sa velikim pocetnim slovom'
+            ]
+        );
+
+        $data['password'] = Hash::make($data['password']);
+        $data['passwordConf'] = Hash::make($data['passwordConf']);
+        $user = new User();
+        $user->create($data);
+
+        return response()->json(['poruka' => 'Uspjesna registracija']);
+    }
+}
--- resources/js/components/Register.vue
+++ resources/js/components/Register.vue
@@ -10,24 +10,42 @@
                 <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="floatingInput" v-model="form.name">
                     <label for="floatingInput">Ime</label>
+                    <p v-if="errors.name" class="text-danger">
+                        {{ errors.name[0] }}
+                    </p>
                 </div>
                 <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="floatingInput" v-model="form.lastName">
                     <label for="floatingInput">Prezime</label>
+                    <p v-if="errors.lastName" class="text-danger">
+                        {{ errors.lastName[0] }}
+                    </p>
                 </div>
                 <div class="form-floating mb-3">
                     <input type="email" class="form-control" id="floatingInput" v-model="form.email">
                     <label for="floatingInput">Email address</label>
+                    <p v-if="errors.email" class="text-danger">
+                        {{ errors.email[0] }}
+                    </p>
                 </div>
                 <div class="form-floating mb-3">
                     <input type="password" class="form-control" id="floatingPassword" v-model="form.password">
                     <label for="floatingPassword">Šifra</label>
+                    <p v-if="errors.password" class="text-danger">
+                        {{ errors.password[0] }}
+                    </p>
                 </div>
                 <div class="form-floating ">
                     <input type="password" class="form-control" id="floatingPassword" v-model="form.passwordConf">
                     <label for="floatingPassword">Ponovi šifru</label>
+                    <p v-if="errors.passwordConf" class="text-danger">
+                        {{ errors.passwordConf[0] }}
+                    </p>
                 </div>
-                <button class="btn btn-primary w-100 mt-4">Registriraj se</button>
+                <button type="submit"  class="btn btn-primary w-100 mt-4">Registriraj se</button>
+                <div v-if="successReg" class="alert alert-success mt-3" role="alert">
+                    {{poruka}}
+                </div>
             </form>
 
         </div>
@@ -36,6 +54,8 @@
 </template>
 
 <script>
+
+import axios from "axios";
 export default {
     data() {
         return {
@@ -49,7 +69,14 @@ export default {
             },
             csrfToken: "",
             POST: "",
+            errors: {},
+            successReg:false,
+            poruka:"",
         }
+    },
+    mounted() {
+        // Pozovite fetchCsrfToken() u metodi mounted()
+        this.fetchCsrfToken();
     },
     methods: {
         fetchCsrfToken() {
@@ -68,7 +95,7 @@ export default {
                 lastName: this.form.lastName,
                 email: this.form.email,
                 password: this.form.password,
-                passConf: this.form.passwordConfirmation,
+                passwordConf: this.form.passwordConf,
             };
             axios.defaults.headers.common["X-CSRF-TOKEN"] = this.csrfToken;
             axios
@@ -81,7 +108,7 @@ export default {
                         lastName: "",
                         email: "",
                         password: "",
-                        passwordConfirmation: "",
+                        passwordConf: "",
                     };
                     this.errors = {};
                 })
